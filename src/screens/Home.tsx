d2b00400347f6a6d9275871f298045c10c5ae120
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-11-30 19:02:33 +0900

    #4.8 Extras
    ## `useSearchParams`
    
    - URL search Parameter를 읽고 수정하는 것을 도와주는 Hook
    
    ### search Parameter
    
    [URLSearchParams - Web API | MDN](https://developer.mozilla.org/ko/docs/Web/API/URLSearchParams)
    
    - 검색 할 때, 필터링, 페이지네이션 등에 사용하는 파라미터
    - 검색, 필터링, 페이지네이션 등의 데이터를 URL에 담는다.
    - 자바스크립트 자체 제공
        - 많은 유틸리티 메서드를 제공
    
    ### 사용법
    
    ```tsx
    import { useSearchParams } from "react-router-dom";
    
    const [readSearchParams, setSearchParams] = useSearchParams();
    ```
    
    - `useSearchParams()` 는 두 아이템을 갖는 Array를 제공한다.
        - 첫번째 아이템은 `SearchParams` 를 **읽기** 위한 것
            - `.get()`, `.has()` 등의 [URL search Parameter 유틸리티 메서드](https://www.notion.so/04-2022-UPDATE-REACT-ROUTER-V6-ad4e27b5fa884ace8c3181f73d1c48fe)를 사용 할 수 있다.
        - 두번째 아이템은 `SearchParams` 를 **수정하기** 위한 것
    
    ## 그 외
    
    [Tutorial v6.4.3](https://reactrouter.com/en/main/start/tutorial)
    
    - react-router 버전 6에서는 Routing 외에도 여러 기능이 추가 되었다.
    - data fetch 도 가능하고
    
    ### **[Loading Data](https://reactrouter.com/en/main/start/tutorial#loading-data)**
    
    - 데이터를 로드 할 수도 있다.
    
    ### [Form](https://reactrouter.com/en/main/start/tutorial#data-writes--html-forms)
    
    - HTML form 처럼 사용 할 수 있다.
    - request에 접근 할 수 있다.
    
    ## Remix 프레임워크
    
    [Remix - Build Better Websites](https://remix.run/)
    
    - react-router를 만든 사람들은 Remix 프레임워크도 만들었다.
    - react-router를 만들때에도 Remix 프레임워크의 컨셉을 참고하였다.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,18 @@
 import { users } from '../db';
-import {Link} from 'react-router-dom';
+import {Link, useSearchParams} from 'react-router-dom';
 
 function Home() {
+    const [readSearchParams, setSearchParams] = useSearchParams();
+
+    setTimeout(() => {
+        setSearchParams({
+            day: 'today',
+            tomorrow: '123',
+        });
+        // console.log(readSearchParams.get('day'));
+        // console.log(readSearchParams.has('day'));
+    }, 2000);
+
     return (
         <>
             <h1>Users</h1>
